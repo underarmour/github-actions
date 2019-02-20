7031447a8078c0ccfffd213dc67cadaea4466ed4
Kyle Rockman <krockman@underarmour.com>
2019-02-20 13:28:33 -0600

    add logging

--- flux-cd/entrypoint.sh
+++ flux-cd/entrypoint.sh
@@ -4,6 +4,8 @@ if [ "$TESTVAR" != "refs/heads/master" ]; then
     exit 0
 fi
 
+echo "[UA] Beginning Flux CD Process"
+
 mkdir -p /flux
 cd /flux
 hub clone underarmour/flux-kubernetes
@@ -16,17 +18,20 @@ OPS_CHINA=$GITHUB_WORKSPACE/ops/china
 if [ -d "$OPS_GLOBAL" ]; then
     mkdir -p /flux/global/automated/$GITHUB_REPOSITORY
     cp -r $OPS_GLOBAL /flux/global/automated/$GITHUB_REPOSITORY
+    echo "[UA] Copying '$OPS_GLOBAL' files to flux"
 fi
 
 if [ -d "$OPS_US" ]; then
     mkdir -p /flux/us/automated/$GITHUB_REPOSITORY
     cp -r $OPS_US /flux/us/automated/$GITHUB_REPOSITORY
+    echo "[UA] Copying '$OPS_US' files to flux"
 fi
 
 if [ -d "$OPS_CHINA" ]; then
     mkdir -p /flux/tokyo/automated/$GITHUB_REPOSITORY
     cp -r $OPS_CHINA /flux/tokyo/automated/$GITHUB_REPOSITORY
+    echo "[UA] Copying '$OPS_CHINA' files to flux"
 fi
 
 cd /flux
-git status
+echo $(git status)
