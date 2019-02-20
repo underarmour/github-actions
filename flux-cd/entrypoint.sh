a4d2f75170985eeb71b2def9e2b39acadd054110
Kyle Rockman <krockman@underarmour.com>
2019-02-20 13:01:25 -0600

    initial test

--- flux-cd/entrypoint.sh
+++ flux-cd/entrypoint.sh
@@ -1,4 +1,28 @@
 #!/bin/sh -l
 
-sh -c "echo $*"
-env
+mkdir -p /flux
+cd /flux
+hub clone underarmour/flux-kubernetes
+cd $GITHUB_WORKSPACE
+
+OPS_GLOBAL=$GITHUB_WORKSPACE/ops/global
+OPS_US=$GITHUB_WORKSPACE/ops/us
+OPS_CHINA=$GITHUB_WORKSPACE/ops/china
+
+if [ -d "$OPS_GLOBAL" ]; then
+    mkdir -p /flux/global/automated/$GITHUB_REPOSITORY
+    cp -r $OPS_GLOBAL /flux/global/automated/$GITHUB_REPOSITORY
+fi
+
+if [ -d "$OPS_US" ]; then
+    mkdir -p /flux/us/automated/$GITHUB_REPOSITORY
+    cp -r $OPS_US /flux/us/automated/$GITHUB_REPOSITORY
+fi
+
+if [ -d "$OPS_CHINA" ]; then
+    mkdir -p /flux/tokyo/automated/$GITHUB_REPOSITORY
+    cp -r $OPS_CHINA /flux/tokyo/automated/$GITHUB_REPOSITORY
+fi
+
+cd /flux
+git status
