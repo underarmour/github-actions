fd522b44698dd8d6e4f89ad08f446b55038cc72b
Kyle Rockman <krockman@underarmour.com>
2019-02-20 13:32:11 -0600

    more fixes

--- flux-cd/entrypoint.sh
+++ flux-cd/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -l
 
-if [ "$TESTVAR" != "refs/heads/master" ]; then
+if [ "$GITHUB_REF" != "refs/heads/master" ]; then
+    echo "[UA] Skipping this commit because its not a commit to the 'master' branch"
     exit 0
 fi
 
-echo "[UA] Beginning Flux CD Process"
+echo "[UA] Beginning Flux CD Process..."
 
 mkdir -p /flux
 cd /flux
@@ -18,20 +19,20 @@ OPS_CHINA=$GITHUB_WORKSPACE/ops/china
 if [ -d "$OPS_GLOBAL" ]; then
     mkdir -p /flux/global/automated/$GITHUB_REPOSITORY
     cp -r $OPS_GLOBAL /flux/global/automated/$GITHUB_REPOSITORY
-    echo "[UA] Copying '$OPS_GLOBAL' files to flux"
+    echo "[UA] Copying '$OPS_GLOBAL' files to flux repo."
 fi
 
 if [ -d "$OPS_US" ]; then
     mkdir -p /flux/us/automated/$GITHUB_REPOSITORY
     cp -r $OPS_US /flux/us/automated/$GITHUB_REPOSITORY
-    echo "[UA] Copying '$OPS_US' files to flux"
+    echo "[UA] Copying '$OPS_US' files to flux repo."
 fi
 
 if [ -d "$OPS_CHINA" ]; then
     mkdir -p /flux/tokyo/automated/$GITHUB_REPOSITORY
     cp -r $OPS_CHINA /flux/tokyo/automated/$GITHUB_REPOSITORY
-    echo "[UA] Copying '$OPS_CHINA' files to flux"
+    echo "[UA] Copying '$OPS_CHINA' files to flux repo."
 fi
 
 cd /flux
-echo $(git status)
+git status
